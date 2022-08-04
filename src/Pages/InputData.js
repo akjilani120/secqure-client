b40779cee96823b5ecce09597e372e53b392717a
unknown <abdulkaderjilani120@gmail.com>
2022-08-05 01:30:30 +0600

    added input style

--- src/Pages/InputData.js
+++ src/Pages/InputData.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import './Style.css'
 const InputData = () => {
+
     return (
-        <div>
-            <h1>I am Input data</h1>
+        <div className='input-data-header'>
+            <div className="input-data-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Date Of Birth</label>
+                        <input type="date" class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Gender</label>
+                        <input type="text" class="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Image</label>
+                        <input type="file" class="form-control" id="exampleInputPassword1" />
+                    </div>                   
+                    
+                    <div class="mb-3">
+                        
+                        <input type="submit" class="form-control" value="Submit" id="exampleInputPassword1" />
+                    </div>
+                    
+                    
+                </form>
+            </div>
         </div>
     );
 };
