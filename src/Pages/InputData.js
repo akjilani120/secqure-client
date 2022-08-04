940ddcf529d557ea883755d1ce607d69d310bd9d
unknown <abdulkaderjilani120@gmail.com>
2022-08-05 02:48:17 +0600

    added react form hook

--- src/Pages/InputData.js
+++ src/Pages/InputData.js
@@ -2,60 +2,59 @@ import React, { useEffect, useState } from 'react';
 import TimePicker from 'react-time-picker';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useForm } from "react-hook-form";
 import './Style.css'
 const InputData = () => {
-
+    const { handleSubmit, register, reset } = useForm();
     const [value, setValue] = useState(new Date());
 
     useEffect(() => {
-      const interval = setInterval(() => setValue(new Date()), 1000);
-  
-      return () => {
-        clearInterval(interval);
-      };
+        const interval = setInterval(() => setValue(new Date()), 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
-    const handleSubmit =(event) =>{
-        event.preventDefault()
-        const name = event.target.name.value
-        const birthDate = event.target.birth.value
-        const gender = event.target.gender.value
-        const image = event.target.image.value 
-       console.log("image" , image)
-    }
+    const onSubmit = data => {
+        console.log("data" , data)
+        console.log("date" , value)
+    };
+    
     return (
         <div className='input-data-header'>
             <div className="input-data-body">
-                 <h2 className='text-center text-primary'>Please Data Add </h2>
-                <form onSubmit={handleSubmit}>
+                <h2 className='text-center text-primary'>Please Data Add </h2>
+                <TimePicker className="d-none" value={value} />
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='name' />
-                            
+                        <input className='form-control' type='text' {...register("name", { required: true })} />
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Date Of Birth</label>
-                        <input type="date" class="form-control" id="exampleInputPassword1" name='birth' />
+                        <label for="exampleInputEmail1" class="form-label">Birth Date</label>
+                        <input className='form-control' type='date' {...register("birthday", { required: true })} />
                     </div>
+                  
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Gender</label>
-                        <input type="text" class="form-control" id="exampleInputPassword1" name='gender' />
+                        <label for="exampleInputEmail1" class="form-label">Gender</label>
+                        <input className='form-control' type='text' {...register("gender")} />
                     </div>
+                  
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Image</label>
-                        <input type="file" class="form-control" id="exampleInputPassword1" name='image' />
-                    </div>                   
-                    <div class="mb-3">
-                      
-                        <TimePicker  value={value} />
-                    </div>                   
-                    
-                    <div class="my-4 mx-auto text-center">
-                        
-                        <input type="submit" class="form-control btn btn-primary w-50 py-2 btn-submit" value="Submit" id="exampleInputPassword1" />
+                        <label for="exampleInputEmail1" class="form-label">Image</label>
+                        <input className='form-control' type='file' {...register("img")} />
                     </div>
-                    
-                    
+                  
+                    <div class="my-4 text-center ">                       
+                    <input className='btn btn-primary py-2 px-3' type="submit" />
+                    </div>
+                  
+                  
+                   
+                   
+                   
                 </form>
+               
             </div>
             <ToastContainer />
         </div>
