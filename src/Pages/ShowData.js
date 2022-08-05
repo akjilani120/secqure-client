98ad93adf2b1fb01def5408de831bd0574843568
unknown <abdulkaderjilani120@gmail.com>
2022-08-05 18:32:14 +0600

    search complete

--- src/Pages/ShowData.js
+++ src/Pages/ShowData.js
@@ -4,33 +4,41 @@ import ShowDataRow from './ShowDataRow';
 const ShowData = () => {
     const [users, setUsers] = useState([])
     const [searchTerm, setSearchTerm] = useState("");
-   
-    const [isLoading , setIsLoading] = useState(false)
+
+    const [isLoading, setIsLoading] = useState(false)
     const handleChange = e => {
         setSearchTerm(e.target.value);
     };
-   
-    useEffect(() => {        
+
+    useEffect(() => {
         fetch("http://localhost:5000/users")
             .then(res => res.json())
-            .then(data => {                
+            .then(data => {
                 setUsers(data)
                 setIsLoading(false)
             })
     }, [])
-    
+
     return (
         <div>
             <div className='container'>
                 <h1 className='text-center text-primary my-4'>Details Data</h1>
-                <div>
+                {/* <div className='my-3 '>
                     <input
+                       className='search-header'
                         type="text"
-                        placeholder="Search"                        
+                        placeholder="Search Name"                        
                         onChange={handleChange}
                     />
+                </div> */}
+                <div className='search-header'>
+                <div class="input-group mb-3  ">
+                    <input  onChange={handleChange} type="text" class="form-control" placeholder="Search Name" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                        <span class="input-group-text" id="basic-addon2">Search</span>
                 </div>
-                
+
+                </div>
+
                 <div class="table-responsive">
                     <table class="table text-center table-striped">
                         <thead>
@@ -44,16 +52,16 @@ const ShowData = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {  isLoading ? 
-                               <h4> Loading .......</h4> :
+                            {isLoading ?
+                                <h4> Loading .......</h4> :
                                 users.filter(value => {
-                                    if(searchTerm === ""){
+                                    if (searchTerm === "") {
                                         return value
-                                    }else if(value.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
-                                            return value
+                                    } else if (value.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                                        return value
                                     }
-                                } )
-                                .map((user, index) => <ShowDataRow  user={user} key={user._id} index={index}></ShowDataRow>)
+                                })
+                                    .map((user, index) => <ShowDataRow user={user} key={user._id} index={index}></ShowDataRow>)
                             }
                         </tbody>
                     </table>
